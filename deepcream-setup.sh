cdabbb77bfef4a1ea39cb6274917235c461fd5a5
Xavier Skittles <75028813+Ivonov1@users.noreply.github.com>
2024-07-03 13:54:01 +0600

    Update deepcream-setup.sh

--- deepcream-setup.sh
+++ deepcream-setup.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 git clone https://gitlab.com/ubcseagull/doujinci.git
-#wget https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh
+wget https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh
 chmod +x Anaconda3-2024.06-1-Linux-x86_64.sh
 ./Anaconda3-2024.06-1-Linux-x86_64.sh -b
 export PATH=$PATH:$HOME/anaconda3/bin
